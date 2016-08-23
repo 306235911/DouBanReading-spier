38f900e781c7f2f35bb84dfcae8cb17d94ee58ca
PWX <306235911@qq.com>
2016-08-23 11:12:46 +0800

    updating the spider
    for the website

--- mysql.py
+++ mysql.py
@@ -13,15 +13,15 @@ class Mysql:
         except MySQldb.Error , e:
             print "连接数据库错误，原因 %d : %s" % (e.args[0] ,e.args[1])
             
-    def insertData(self, BookType, identity, name, info , score, num):
+    def insertData(self, BookType, identity, name, info , score, num , bookUrl, imgUrl):
         # sql = "INSERT INTO book VALUES (%d,%s,%s,%s,%s)" % (identity , name , info , score , num)
-        sql = "INSERT INTO %s VALUES " % BookType + "(%s,%s,%s,%s,%s)"
+        sql = "INSERT INTO book VALUES " + "(%s,%s,%s,%s,%s,%s,%s,%s)"
         try:
             self.db.set_character_set('utf8')
             # result = self.cur.execute(sql)
             
             # 比起 "INSERT INTO table VALUES(%s,%s,%s)" % (a,b,c) 的用法，更常用的为下面的表达
-            result = self.cur.execute(sql,(identity , name , info , score , num))
+            result = self.cur.execute(sql,(identity , name , info , score , num , BookType , bookUrl , imgUrl))
             # insert_id = self.db.insert_id()
             self.db.commit()
             if result:
@@ -36,13 +36,16 @@ class Mysql:
                 print "插入数据失败，原因 %d : %s" % (e.args[0] ,e.args[1])
     
     # 创建数据库表            
-    def createTable(self , table):
-        sql = """CREATE TABLE %s (
-        identity INT UNSIGNED NOT NULL PRIMARY KEY,
-        name VARCHAR(30) NOT NULL,
-        info VARCHAR(65) NOT NULL,
-        score VARCHAR(15) NOT NULL,
-        num VARCHAR(20) NOT NULL);""" %  table
+    def createTable(self):
+        sql = """CREATE TABLE book (
+        identity INT(10) UNSIGNED NOT NULL PRIMARY KEY,
+        name CHAR(60) NOT NULL,
+        info CHAR(90) NOT NULL,
+        score CHAR(10) NOT NULL,
+        num CHAR(20) NOT NULL,
+        bookType CHAR(40) NOT NULL,
+        bookUrl CHAR(60) NOT NULL,
+        imgUrl CHAR(60) NOT NULL);""" 
         # drop = "DROP TABLE IF EXISTS %s" % table
  
         try:
@@ -51,7 +54,7 @@ class Mysql:
             print u"创建数据库表成功"
         except MySQLdb.Error,e:
             self.db.rollback()
-            print "创建数据库表失败,原因 %d : %s" % (e.args[0] , e.args[1])
+            print u"创建数据库表失败,原因" + " %d : %s" % (e.args[0] , e.args[1])
         except:
             print u"数据库语言错误"
             
